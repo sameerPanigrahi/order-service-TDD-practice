8134d328dfc328f6634796ed6bed56fe4bedf535
sameer <here.i.am.sam@gmail.com>
2019-04-21 19:42:34 +0530

    Using fixtures and basic Mockito when/then's

--- src/test/java/com/bigbox/b2csite/order/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/bigbox/b2csite/order/service/impl/OrderServiceImplTest.java
@@ -1,15 +1,29 @@
 package com.bigbox.b2csite.order.service.impl;
 
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import com.bigbox.b2csite.order.dao.OrderDao;
+import com.bigbox.b2csite.order.model.domain.OrderSummary;
+import com.bigbox.b2csite.order.model.entity.OrderEntity;
+import com.bigbox.b2csite.order.model.entity.OrderItemEntity;
 import com.bigbox.b2csite.order.model.transformer.OrderEntityToOrderSummaryTransformer;
 
 public class OrderServiceImplTest {
+
+	private final static long CUSTOMER_ID = 1;
+	private final static Long ORDER_ID = 2L;
+	private final static Long ITEM_ID = 3L;
+	private final static String ORDER_NUMBER = "54";
 
 	private OrderServiceImpl target = null;
 
@@ -26,14 +40,36 @@ public class OrderServiceImplTest {
 	}
 
 	@Test
-	@Ignore
 	public void test_getOrderSummary_success() throws Exception {
 
 		// Setup
+		OrderEntity orderEntityFixture = new OrderEntity();
+		orderEntityFixture.setCustomerId(CUSTOMER_ID);
+		orderEntityFixture.setId(ORDER_ID);
+		orderEntityFixture.setOrderNumber(ORDER_NUMBER);
+		orderEntityFixture.setOrderItemList(new LinkedList<OrderItemEntity>());
+
+		OrderItemEntity orderItemFixture = new OrderItemEntity();
+		orderItemFixture.setId(ITEM_ID);
+		orderItemFixture.setOwningOrder(orderEntityFixture);
+		orderItemFixture.setSellingPrice(new BigDecimal("1.5"));
+		orderItemFixture.setQuantity(2);
+
+		orderEntityFixture.getOrderItemList().add(orderItemFixture);
+
+		List<OrderEntity> orderEntityListFixture = new LinkedList<>();
+		orderEntityListFixture.add(orderEntityFixture);
+
+		Mockito.when(mockOrderDao.findOrdersByCustomer(CUSTOMER_ID)).thenReturn(orderEntityListFixture);
+		Mockito.when(mockTransformer.transform(orderEntityFixture)).thenCallRealMethod();
 
 		// Execution
+		List<OrderSummary> result = target.getOrderSummary(CUSTOMER_ID);
 
 		// Verification
+		Assert.assertNotNull(result);
+		Assert.assertEquals(1, result.size());
+		Assert.assertEquals(2, result.get(0).getItemCount());
 	}
 
 	@Test
